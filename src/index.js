28b64fead85981beb757bbbeaa3253e9a5a7b7dc
Andrew Gross <grossmail1@gmail.com>
2018-09-09 10:56:09 -0600

    reducers and action creators

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from 'reducers'
 
 import App from 'components/App'
 
-ReactDom.render(<App/>, document.getElementById('root'))
+ReactDom.render(
+  <Provider store={createStore(reducers, {})}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'))
